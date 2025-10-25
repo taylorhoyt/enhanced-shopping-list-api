8a33c2864718b0bf583621337b457d7c90a43e42
Taylor Hoyt <taych2002@gmail.com>
2025-10-25 13:41:02 -0500

    update list controller

--- src/controllers/listController.ts
+++ src/controllers/listController.ts
@@ -14,3 +14,42 @@ export const getAllLists = async (req: Request, res: Response) => {
     return res.status(500).json({ error: "Error getting lists" });
   }
 };
+
+export const getListById = async (req: Request, res: Response) => {
+  const listService = new ListService();
+  try {
+    const supabase = getUserSupabaseClient(req.headers.authorization!);
+    const data = await listService.getListById(supabase, req.params.id);
+
+    return res.status(200).json(data);
+  } catch (error: any) {
+    console.error("Error getting list:", error.message);
+    return res.status(500).json({ error: "Error getting list" });
+  }
+};
+
+export const getListItems = async (req: Request, res: Response) => {
+  const listService = new ListService();
+  try {
+    const supabase = getUserSupabaseClient(req.headers.authorization!);
+    const data = await listService.getListItems(supabase, req.params.id);
+
+    return res.status(200).json(data);
+  } catch (error: any) {
+    console.error("Error getting list items:", error.message);
+    return res.status(500).json({ error: "Error getting list items" });
+  }
+};
+
+export const getListMeals = async (req: Request, res: Response) => {
+  const listService = new ListService();
+  try {
+    const supabase = getUserSupabaseClient(req.headers.authorization!);
+    const data = await listService.getListMeals(supabase, req.params.id);
+
+    return res.status(200).json(data);
+  } catch (error: any) {
+    console.error("Error getting list meals:", error.message);
+    return res.status(500).json({ error: "Error getting list meals" });
+  }
+};
