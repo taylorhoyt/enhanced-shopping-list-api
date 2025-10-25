d375282965a75b52e0a2a074f5cde7b33524731b
Taylor Hoyt <taych2002@gmail.com>
2025-10-25 12:54:39 -0500

    use get authenticated client method

--- src/services/itemService.ts
+++ src/services/itemService.ts
@@ -1,20 +1,9 @@
-import { createClient } from "@supabase/supabase-js";
+import { Database } from "@/types/supabase";
+import { SupabaseClient } from "@supabase/supabase-js";
 
 export class ItemService {
-  private supabaseUrl: string;
-  private supabaseAnonKey: string;
-
-  constructor() {
-    this.supabaseUrl = process.env.SUPABASE_URL!;
-    this.supabaseAnonKey = process.env.SUPABASE_ANON_KEY!;
-  }
-
-  async getAllItems(token: string) {
-    const supabaseUser = createClient(this.supabaseUrl, this.supabaseAnonKey, {
-      global: { headers: { Authorization: `Bearer ${token}` } },
-    });
-
-    const { data, error } = await supabaseUser
+  async getAllItems(supabase: SupabaseClient<Database>) {
+    const { data, error } = await supabase
       .schema("base_schema")
       .from("item")
       .select("id, name, category, created_at, created_by");
