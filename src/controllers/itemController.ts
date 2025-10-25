c745673f31e420e2a67780331c67a20b731a4292
Taylor Hoyt <taych2002@gmail.com>
2025-10-25 12:54:21 -0500

    use get authenticated client method

--- src/controllers/itemController.ts
+++ src/controllers/itemController.ts
@@ -1,16 +1,12 @@
 import { Request, Response } from "express";
 import { ItemService } from "@/services/itemService";
-
-const itemService = new ItemService();
+import { getUserSupabaseClient } from "@/database/database";
 
 export const getAllItems = async (req: Request, res: Response) => {
+  const itemService = new ItemService();
   try {
-    if (!req.headers.authorization) {
-      return res.status(401).json({ error: "No token provided" });
-    }
-
-    const token = req.headers.authorization.split(" ")[1];
-    const data = await itemService.getAllItems(token);
+    const supabase = getUserSupabaseClient(req.headers.authorization!);
+    const data = await itemService.getAllItems(supabase);
 
     return res.status(200).json(data);
   } catch (error: any) {
