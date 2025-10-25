7f28879285d054bd4b31e9e6d5ce6388767ce6b9
Taylor Hoyt <taych2002@gmail.com>
2025-10-25 13:41:08 -0500

    update list routes

--- src/routes/listRoutes.ts
+++ src/routes/listRoutes.ts
@@ -1,9 +1,14 @@
 import { Router } from "express";
 import { authenticateUser } from "@/middleware/auth";
 import { getAllLists } from "@/controllers/ListController";
+import { getListById } from "@/controllers/ListController";
+import { getListItems } from "@/controllers/ListController";
+import { getListMeals } from "@/controllers/ListController";
 
 const router = Router();
 
 router.get("/", authenticateUser, getAllLists);
-
+router.get("/:id", authenticateUser, getListById);
+router.get("/:id/items", authenticateUser, getListItems);
+router.get("/:id/meals", authenticateUser, getListMeals);
 export default router;
